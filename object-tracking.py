11edc26be7f055ca920948c291c24cfe42c136f0
Aldo Vargas <alduxvm@gmail.com>
2015-02-06 18:25:49 +0000

    Object tracking
    New object tracking algorithm

---
+++ object-tracking.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""object-tracking.py: Object detection using openCV."""
+
+__author__ = "Aldo Vargas"
+__copyright__ = "Copyright 2015 Aldux.net"
+
+__license__ = "GPL"
+__version__ = "1"
+__maintainer__ = "Aldo Vargas"
+__email__ = "alduxvm@gmail.com"
+__status__ = "Development"
+
+import numpy as np
+import cv2
+
+def run_main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3,320)
+    cap.set(4,240)
+
+    # Read the first frame of the video
+    ret, frame = cap.read()
+
+    # Set the ROI (Region of Interest). Actually, this is a
+    # rectangle of the building that we're tracking
+    c,r,w,h = 160,120,30,30
+    track_window = (c,r,w,h)
+
+    # Create mask and normalized histogram
+    roi = frame[r:r+h, c:c+w]
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_roi, np.array((0., 30.,32.)), np.array((180.,255.,255.)))
+    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 80, 1)
+    
+    while True:
+        t = cv2.getTickCount()
+        ret, frame = cap.read()
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0,180], 1)
+
+        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+
+        x,y,w,h = track_window
+        cv2.rectangle(frame, (x,y), (x+w,y+h), 255, 2)
+        cv2.putText(frame, 'Tracked', (x-25,y-10), cv2.FONT_HERSHEY_SIMPLEX,
+            .5, (255,255,255), 1, cv2.CV_AA)
+        
+        t = cv2.getTickCount() - t
+        print "detection time = %gms" % (t/(cv2.getTickFrequency()*1000.))
+        cv2.imshow('Tracking', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    run_main()
